2961d0acf8f266bc7fbd3d901a9c6462a3fcfcf6
igorjba <igorjbahia@gmail.com>
2022-11-20 07:50:13 -0300

    :recycle: refactor: Adição de comentário com outra forma de criar variáveis, desta vez, todas na mesma linha.

--- 05/index.js
+++ 05/index.js
@@ -4,5 +4,7 @@ const x2 = 1;
 const y1 = 1;
 const y2 = 4;
 
+//const x1 = 1, x2 = 1, y1 = 1, y2 = 4;
+
 const distanciaEntreDoisPontos = d = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 console.log(`A distância entre esses 2 pontos dados é de ${distanciaEntreDoisPontos}.`);
