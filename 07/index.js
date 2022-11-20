af6b4aa17979c895fd883e58c5503f01cab2303d
igorjba <igorjbahia@gmail.com>
2022-11-20 08:06:00 -0300

    :recycle: refactor: Adição de comentário sobre assunto da questão,ajuste do tempo percorrido de 7 para 100 e substituição do nome da varável P0 para ajuste conforme convenção.

--- 07/index.js
+++ 07/index.js
@@ -1,6 +1,8 @@
-const populacaoInicial = Po = 1_000; //população incial de pessoas infectadas
-const quantidadeRisco = x = 4; //quantidade de pessoas para as quais um paciente infectado pode transmitir
-const tempoPercorrido = t = 7; //dias
-const simulacao = Po * x ** (t / 7);
+//Curva Exponencial
 
-console.log(`Visto que inicialmente havia ${Po} pessoas infectadas, ${simulacao} será(ão) infectada(s) após ${t} dia(s).`)
+const populacaoInicial = p0 = 1_000; //população incial de pessoas infectadas
+const quantidadeRisco = x = 4; //quantidade de pessoas para as quais um paciente infectado pode transmitir
+const tempoPercorrido = t = 100; //dias
+const simulacao = p0 * x ** (t / 7);
+
+console.log(`Visto que inicialmente havia ${p0} pessoas infectadas, ${simulacao} será(ão) infectada(s) após ${t} dia(s).`)
