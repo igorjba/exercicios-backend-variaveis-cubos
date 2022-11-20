31de7dbb8320c17627892ea619355eec0d62f6d6
igorjba <igorjbahia@gmail.com>
2022-11-20 08:13:52 -0300

    :recycle: refactor: Adição de comentário sobre o assunto da questão. Adição de outra forma de encontrar PI.

--- 08/index.js
+++ 08/index.js
@@ -1,4 +1,7 @@
-const pi = 3.14159265359;
+//Cálculo do perimetro e área de um círculo
+
+//const pi = 3.14159265359;
+const pi = Math.PI
 const r = raio = 1;
 const comprimentoDoCirculo = c = 2 * pi * r;
 const areaDoCirculo = a = pi * r ** 2;
