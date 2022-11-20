44f00a3f47d578d68d620a1ed824b08fbdad451d
igorjba <igorjbahia@gmail.com>
2022-11-20 08:22:21 -0300

    :recycle: refactor: Ajuste da fórmula de Baskhara.

--- 10/index.js
+++ 10/index.js
@@ -1,6 +1,6 @@
-const a = 5;
-const b = 5;
-const c = 1;
+const a = 1;
+const b = -5;
+const c = 3;
 
-const delta = x = a ** 2 - 4 * a * c; //Báskhara => 5²-4ac
+const delta = b ** 2 - 4 * a * c; //Báskhara => b²-4ac
 console.log(`O valor de delta é ${delta}.`);
