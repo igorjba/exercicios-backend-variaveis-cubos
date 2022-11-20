f6df78c86d486cdc084a4d415478d4715f5daf76
igorjba <igorjbahia@gmail.com>
2022-11-20 08:43:04 -0300

    :recycle: refactor: Adição da propriedade de pi na variavel pi. Impressão do resultado inserindo o valor de pi no cálculo.

--- 13/index.js
+++ 13/index.js
@@ -1,8 +1,8 @@
-const raioDaBase = r = 3; //raio da base do cilindro
-const alturaDoCilindro = h = 1 //altura do cilindro
-const pi = 3.14159265359;
+const raioDaBase = r = 2; //raio da base do cilindro
+const alturaDoCilindro = h = 3; //altura do cilindro
+const pi = Math.PI;
 
-const areaTotalDoCilindroComPi = At = 2 * pi * r * (r + h);
-const areaTotalDoCilindroSemPi = At = 2 * r * (r + h);
+const areaTotalDoCilindroComPi = at = 2 * pi * r * (r + h);
+const areaTotalDoCilindroSemPi = at = 2 * r * (r + h);
 
-console.log(`Visto que um cilindro tem como raio da base ${raioDaBase} e como altura ${alturaDoCilindro}. Ao calcularmos a  área total deste cilindro, o resultado será ≅ ${areaTotalDoCilindroSemPi}pi.`);
+console.log(`Visto que um cilindro tem como raio da base ${raioDaBase} e como altura ${alturaDoCilindro}. Ao calcularmos a  área total deste cilindro, o resultado será ≅ ${areaTotalDoCilindroSemPi}pi, ou ≅ ${areaTotalDoCilindroComPi.toFixed(2)}.`);
