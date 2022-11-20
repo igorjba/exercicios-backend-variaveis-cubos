18a6f750bd6437ca0adb3eac98d45eb9fba347b8
igorjba <igorjbahia@gmail.com>
2022-11-20 06:57:11 -0300

    :recycle: refactor: Ajuste to texto impresso e adicionada outra formar de trabalhar com potência.

--- 01/index.js
+++ 01/index.js
@@ -2,5 +2,6 @@ const peso = 76;
 const altura = 1.79;
 
 const imc = peso / altura ** 2;
+//const imc = pese / Math.pow(altura, 2);
 
-console.log(`O IMC é ${imc}`);
+console.log(`Seu IMC é ${imc}.`);
